d1cf98bfd38fc9c1443b56a35c3ae62981fb10d1
Adam <ajsyav@gmail.com>
2016-01-26 19:20:54 -0600

    Create redirect.php
    made redirect.php

---
+++ redirect.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>redirect</title>
+    </head>
+    <body>
+        <?php
+            header("Location: signup.html");//redirect from front page to the sign up page
+        ?>
+    </body>
+</html>
